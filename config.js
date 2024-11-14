7cba6c2cf9b540c93ea2eb8838cdd69b901f4ab8
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-11-14 12:13:20 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- config.js
+++ config.js
@@ -5,19 +5,19 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "WqhlyCCZ#iHv4Y8dDxirRUNfFlT2CioY_akYiHGCT8-U4GVHvgGo",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/7NJbnB3/lo75s-RTo4294.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M MANNO-MD WHATSAPP BOT 😊♻️",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+BOT_NAME: process.env.BOT_NAME || "『💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌』 ",
 };
