8003679e763d5932d13cefdda708eff06d396355
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-11-14 11:17:35 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/loli.js
+++ plugins/loli.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SILENT-SOBX-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-SOBX-MD BY SILENTLOVER432*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '*💌 𝙈𝘼𝙉𝙉𝙊-𝙈𝘿-𝙎𝙀𝙇𝙀𝘾𝙏 𝙂𝙄𝙍𝙇𝙎 𝙄𝙈𝘼𝙂𝙀___ 💌*\n\n\n *🧬© 𝙂𝙍𝘼𝙉𝙏𝙀𝘿 𝘽𝙔 𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 📦*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
